f92a6f7e24902bf37c5230e16cf52f2eb003b756
fernandomaraujo <fernandomarinho.araujo@gmail.com>
2021-05-21 18:58:28 -0300

    Introdução inicial a hooks

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 
 import './styles.css';
 
@@ -7,118 +7,109 @@ import { Posts } from '../../components/Posts';
 import { Button } from '../../components/Button';
 import { TextInput } from '../../components/TextInput';
 
-export class Home extends Component {
-  
-  state = {
-    posts: [],
-    allPosts: [],
-    page: 0,
-    postsPerPage: 10,
-    searchValue: ''
-  };
-  
-  async componentDidMount() {
-    
-    await this.loadPosts();
-  }
+export const Home = () => {
 
-  loadPosts = async() => {
+  // Hook
+  // Desestruturação do array
+  const [posts, setPosts] = useState([]);
+  const [allPosts, setAllPosts] = useState([]);
+  const [page, setPage] = useState(0);
+  const [postsPerPage] = useState(10);
+  const [searchValue, setSearchValue] = useState('');
 
-    const { page, postsPerPage } = this.state;
-    const postsAndPhotos = await loadPosts();
+  const noMorePosts = page + postsPerPage >= allPosts.length;
 
-    this.setState({ 
-      posts: postsAndPhotos.slice(page,postsPerPage),
-      allPosts: postsAndPhotos,
-    });
-  }
-
-  loadMorePosts = () => {
-    
-    const {
-      page,
-      postsPerPage,
-      allPosts,
-      posts
-    } = this.state;
-
-    const nextPage = page + postsPerPage;
-    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
-
-    posts.push(...nextPosts);
-
-    this.setState({ posts, page: nextPage });
-
-  }
-
-  handleChange = (e) => {
-    const {value} = e.target;
-
-    this.setState({ searchValue: value });
-  }
-
-  render() {
-
-    const { posts, page, postsPerPage, allPosts, searchValue } = this.state;
-    const noMorePosts = page + postsPerPage >= allPosts.length;
-
-    // Operação condicional ternário
-    // Se tem valor, filtra os posts, caso não retorna posts normal
-    const filteredPosts = !!searchValue ? 
+  // Operação condicional ternário
+  // Se tem valor, filtra os posts, caso não retorna posts normal
+  const filteredPosts = !!searchValue ? 
     
     allPosts.filter(post => {
 
       return post.title.toLowerCase()
         .includes(searchValue.toLocaleLowerCase());
 
-    })
-    : posts;
+  })
+  : posts;
+
+  const handleLoadPosts = useCallback(async(page, postsPerPage) => {
+
+    const postsAndPhotos = await loadPosts();
+
+    setPosts(postsAndPhotos.slice(page, postsPerPage));
+    setAllPosts(postsAndPhotos);
+  }, []);
+
+  useEffect(() => {
+
+    handleLoadPosts(0, postsPerPage);
+    
+  }, [handleLoadPosts, postsPerPage]);
+
+  const loadMorePosts = () => {
+  
+    const nextPage = page + postsPerPage;
+    const nextPosts = allPosts.slice(nextPage, nextPage + postsPerPage);
+
+    posts.push(...nextPosts);
+
+    setPosts(posts);
+    setPage(nextPage);
+
+  }
+
+  const handleChange = (e) => {
+    const {value} = e.target;
+
+    setSearchValue(value);
+  }
+
+  return(
 
     // O que está sendo exibido 
-    return (
-      <section className="container">
+    
+    <section className="container">
 
-        <div className="search-container">
+      <div className="search-container">
 
-          {/* Avaliação de Curto-Circuito */}
-          {!!searchValue && (
-            <>
-            <h1>Search value: {searchValue}</h1>
-            </>
-          )}
+        {/* Avaliação de Curto-Circuito*/}
+        {!!searchValue && (
+          <>
+          <h1>Search value: {searchValue}</h1>
+          </>
+        )}
 
-          <TextInput 
-            searchValue={searchValue}
-            handleChange={this.handleChange}
+        <TextInput 
+          searchValue={searchValue}
+          handleChange={handleChange}
+        />
+
+      </div>
+
+      {/* Se tiver posts filtrados, exibe */}
+      {filteredPosts.length > 0 && (
+        <Posts posts={filteredPosts} />
+      )}
+
+      {/* Caso contrário */}
+      {filteredPosts.length === 0 && (
+        <p className="without-posts">Não existem posts.</p>
+      )}
+
+      <div className="button-container">
+
+        {/*Se não estiver numa busca, exibir botão */}
+        {!searchValue && (
+          <Button 
+            text="Load more posts"
+            onClick={loadMorePosts}
+            disabled={noMorePosts} 
           />
-
-        </div>
-
-        {/* Se tiver posts filtrados, exibe */}
-        {filteredPosts.length > 0 && (
-          <Posts posts={filteredPosts} />
         )}
-
-        {/* Caso contrário */}
-        {filteredPosts.length === 0 && (
-          <p className="without-posts">Não existem posts.</p>
-        )}
-
-        <div className="button-container">
-
-          {/* Se não estiver numa busca, exibir botão */}
-          {!searchValue && (
-            <Button 
-              text="Load more posts"
-              onClick={this.loadMorePosts}
-              disabled={noMorePosts} 
-            />
-          )}
           
-        </div>
+      </div>
 
-      </section>
-    );
-  }
+    </section>
+  
+  );
 
 }
