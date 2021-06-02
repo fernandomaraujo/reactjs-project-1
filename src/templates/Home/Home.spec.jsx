3c8411e5544a507882cc2dbfc82c5921dbc5673b
fernandomaraujo <fernandomarinho.araujo@gmail.com>
2021-06-02 17:08:03 -0300

    Testes de renderização, busca por posts e botão

--- src/templates/Home/Home.spec.jsx
+++ src/templates/Home/Home.spec.jsx
@@ -1,3 +1,4 @@
+import userEvent from '@testing-library/user-event';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
 
@@ -62,17 +63,105 @@ describe('<Home />', () => {
     server.close();
   });
 
-  afterAll
-
+  
   it('should render search, posts and load more', async () => {
     
     render(<Home />);
     const noMorePosts = screen.getByText('Não existem posts.');
 
+    expect.assertions(3);
+
     await waitForElementToBeRemoved(noMorePosts);
-    screen.debug();
+    //screen.debug();
+
+    const search = screen.getByPlaceholderText(/type your search/i);
+    expect(search).toBeInTheDocument();
+
+    const images = screen.getAllByRole('img', { name: /title/i} );
+    expect(images).toHaveLength(2);
+
+    const button = screen.getByRole('button', { name: /load more posts/i} );
+    expect(button).toBeInTheDocument();
 
   });
+
+  it('should search for posts', async () => {
+    
+    render(<Home />);
+    const noMorePosts = screen.getByText('Não existem posts.');
+
+    expect.assertions(7);
+
+    await waitForElementToBeRemoved(noMorePosts);
+
+    // Se os posts estão sendo exibidos
+    const search = screen.getByPlaceholderText(/type your search/i);
+    
+    expect(screen.getByRole('heading', { name: 'title1 1' }))
+      .toBeInTheDocument();
+
+    expect(screen.getByRole('heading', { name: 'title2 2' }))
+      .toBeInTheDocument();
+
+    // Se unicamente o title1 está sendo exibido
+    userEvent.type(search, 'title1');
+
+    expect(screen.getByRole('heading', { name: 'title1 1' }))
+      .toBeInTheDocument();
+
+    expect(screen.queryByRole('heading', { name: 'title2 2' }))
+      .not.toBeInTheDocument();
+
+    // Se o que está sendo digitado, está sendo exibido
+    expect(screen.getByRole('heading', { name: 'Search value: title1' }));
+    
+    // Se search está limpo e os post voltarão a serem exibidos
+    userEvent.clear(search);
+
+    expect(screen.getByRole('heading', { name: 'title1 1' }))
+      .toBeInTheDocument();
+
+    expect(screen.queryByRole('heading', { name: 'title2 2' }))
+      .toBeInTheDocument();
+
+    // Se algo pesquisado não existindo, é retornado a mensagem sobre não ter posts
+    userEvent.type(search, 'post does not exist');
+    expect(screen.getByText('Não existem posts.'))
+      .toBeInTheDocument();
+
+  });
+
+  it('should load more posts', async () => {
+    
+    render(<Home />);
+    const noMorePosts = screen.getByText('Não existem posts.');
+
+    // expect.assertions(3);
+
+    await waitForElementToBeRemoved(noMorePosts);
+
+    const button = screen.getByRole('button', { name: /load more posts/i} );
+    
+    // Botão clicado, deve carregar mais posts
+    userEvent.click(button);
+
+    expect(screen.getByRole('heading', { name: 'title3 3' }))
+      .toBeInTheDocument();
+
+    // Se não tem mais posts, se o botão está desativado
+    expect(button).toBeDisabled();
+
+  });
+  /**
+   * Ao final, mudar valor de 2 para 10 no arquivo: index.jsx
+   * const [postsPerPage] = useState(2);
+   * 
+   * Depois, alterar para 3 na linha 73:
+   * expect.assertions(2);
+   * 
+   * E alterar pra 3 na linha 81:
+   * expect(images).toHaveLength(2);
+   */
 
 });
 
